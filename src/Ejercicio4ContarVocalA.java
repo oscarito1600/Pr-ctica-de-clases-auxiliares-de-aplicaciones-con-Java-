d4c4af04d20f927624242c331be35fe712d76eff
Oscar Ramirez <barleyunmain@gmail.com>
2026-05-26 18:36:59 -0600

    Feat: Agregar el ejercicio 5  Realizado por : Oscar Ramirez

---
+++ src/Ejercicio4ContarVocalA.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ejercicio4ContarVocalA {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Ingrese una frase:");
+        String frase = entrada.nextLine();
+
+        int contador = 0;
+
+
+        frase = frase.toUpperCase();
+
+
+        for (int i = 0; i < frase.length(); i++) {
+
+            if (frase.charAt(i) == 'A') {
+                contador++;
+            }
+        }
+
+        System.out.println("La vocal A aparece " + contador + " veces.");
+    }
+}
